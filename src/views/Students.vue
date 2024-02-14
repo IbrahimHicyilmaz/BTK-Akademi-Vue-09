0777e6c5f269b881c9dfc7796dc69dc17539ae3f
IbrahimHicyilmaz <blog.ibrahimhicyilmaz5334@gmail.com>
2024-02-14 15:42:47 +0300

    09 commit

---
+++ src/views/Students.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Öğrenciler</h1>
+        <div v-for="student in students" :key="student.id">
+            <router-link :to="{name:'studentInfo',params:{id:student.id}}">{{ student.name }}</router-link>
+        </div>
+        <div>
+    
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            students: [
+                { id: 1, name: 'ibrahim', not: '95' },
+                { id: 2, name: 'Varol', not: '85' },
+                { id: 3, name: 'Ali', not: '75' },
+            ]
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
